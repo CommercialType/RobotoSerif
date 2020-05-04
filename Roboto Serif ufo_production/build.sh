81095fbad9c63bbff01bc041dd3365883ff0d044
Mark Record <mark@commercialtype.com>
2020-05-04 15:43:44 -0400

    Continued Development

--- Roboto Serif ufo_production/build.sh
+++
@@ -1,9 +0,0 @@
-# build variable fonts
-fontmake -o variable -m roman/Roboto\ Serif\ Variable\ Romans\ 200423.designspace
-fontmake -o variable -m italic/Roboto\ Serif\ Variable\ Italics\ 200423.designspace
-
-# build web fonts
-sfnt2woff-zopfli variable_ttf/Roboto\ Serif\ Variable\ Romans\ 200423-VF.ttf
-woff2_compress variable_ttf/Roboto\ Serif\ Variable\ Romans\ 200423-VF.ttf
-sfnt2woff-zopfli variable_ttf/Roboto\ Serif\ Variable\ Italics\ 200423-VF.ttf
-woff2_compress variable_ttf/Roboto\ Serif\ Variable\ Italics\ 200423-VF.ttf
